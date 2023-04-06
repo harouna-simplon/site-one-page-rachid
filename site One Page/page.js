d52907d5a6b4774dc64b47dbc482eccab0910483
harouna-simplon <128226155+harouna-simplon@users.noreply.github.com>
2023-04-06 17:18:47 +0000

    Add files via upload

---
+++ site One Page/page.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+   // récupérer les valeurs des champs du formulaire
+   const nom = document.getElementById('name').value;
+   const email = document.getElementById('email').value;
+   const telephone = document.getElementById('number').value;
+   const message = document.getElementById('message').value;
+    // validation des champs
+  if (!nom || !email || !telephone || !message) {
+    alert('Veuillez remplir tous les champs du formulaire.');
+    return;
+  }
+  else{
+    alert('Merci pour le contact. Nous vous contacterons sous peu ! localeeat MANGER SAIN VIVEZ MIEUX');
+    form.submit();
+
+  }
+ 
+  
+  // afficher le message de confirmation
+  
+  
+  // réinitialiser le formulaire
+  form.reset();  
+});
+
+   
+   
